c5ed6057d9ce912312f842c594a96db13f541530
shreyas pagar <130757999+shreyaspagar@users.noreply.github.com>
2024-12-08 22:36:25 +0530

    Add files via upload

---
+++ oddeven2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Oddeven2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+//        if(num % 2 == 0) {
+//            System.out.println("even");
+//
+//        }
+//        else {
+//            System.out.println("odd");
+//        }
+
+        String ans;
+        ans = (num % 2 == 0) ? "even" : "odd";
+        System.out.println(ans);
+
+
+
+
+
+
+
+    }
+
+}
